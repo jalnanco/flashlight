d84138b315e2d53c71d4fd8b508fa3257a09a73a
jiho.jang <jihobada@naver.com>
2022-05-12 19:45:56 +0900

    lint: 정렬

--- src/Contacts.svelte
+++ src/Contacts.svelte
@@ -1,4 +1,7 @@
-<h1>
-<p>email: jihobada@naver.com</p>
-<a href="https://jalnanco.notion.site/5ff302c544ef4e579cdfa9a443de6708">NOTION 이력서</a>
-</h1>
+<div>
+  <h1>Contacts</h1>
+  email: jihobada@naver.com
+  <a href="https://jalnanco.notion.site/5ff302c544ef4e579cdfa9a443de6708"
+    >NOTION 이력서</a
+  >
+</div>
--- src/MouseHover.svelte
+++ src/MouseHover.svelte
@@ -1,43 +1,54 @@
 <script>
-import { noop } from "svelte/internal";
+  import { noop } from "svelte/internal";
+  let m = { x: 0, y: 0 };
+  let lightElement;
+  let overlayElement;
 
-
-    let m = {x:0, y:0};
-    let lightElement;
-    let overlayElement;
-
-
-    $: overlayElement && overlayElement.style.setProperty('display', 'none')
-    $: lightElement && lightElement.style.setProperty('background', 'radial-gradient(circle at ' + m.x + 'px ' + m.y + 'px, transparent, #000 10%)')
+  $: overlayElement && overlayElement.style.setProperty("display", "none");
+  $: lightElement &&
+    lightElement.style.setProperty(
+      "background",
+      "radial-gradient(circle at " +
+        m.x +
+        "px " +
+        m.y +
+        "px, transparent, #000 10%)"
+    );
 </script>
 
+<div>
+  <h1>MOUSE HOVER FLASH LIGHT TEST</h1>
+  The section mouse position is x: {m.x} y: {m.y}
+  <section
+    on:mousemove={(e) =>
+      (m = {
+        x: e.clientX - e.currentTarget.getBoundingClientRect().x,
+        y: e.clientY - e.currentTarget.getBoundingClientRect().y,
+      })}
+  >
+    <div class="overlay" bind:this={overlayElement} />
+    <div class="light" bind:this={lightElement} />
+  </section>
+</div>
+
 <style>
-    section {
+  section {
     position: relative;
     width: 100%;
     height: 100vh;
-    background: url('https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+    background: url("https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
     background-size: cover;
     overflow: hidden;
-    }
-    .overlay {
+  }
+  .overlay {
     background: #000;
     width: 100%;
     height: 100vh;
     /* display: none; */
-    }
-    .light {
+  }
+  .light {
     position: absolute;
     width: 100%;
     height: 100%;
-    }
+  }
 </style>
-<div>
-    <h1>MOUSE HOVER FLASH LIGHT TEST</h1>
-    The section mouse position is x: {m.x} y: {m.y}
-    <section on:mousemove="{e => m = {x: e.clientX - e.currentTarget.getBoundingClientRect().x, 
-        y: e.clientY - e.currentTarget.getBoundingClientRect().y}}">
-        <div class="overlay" bind:this={overlayElement}></div>
-        <div class="light" bind:this={lightElement}></div>
-    </section>
-</div>
